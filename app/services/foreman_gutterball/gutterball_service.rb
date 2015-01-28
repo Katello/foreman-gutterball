b7683493e9a7f7425fdb1e1401c85e883c26a972
Christine Fouant <cfouant@redhat.com>
2015-01-28 17:25:22 -0500

    fixes #9154 - Adds capability to generate reports

--- app/services/foreman_gutterball/gutterball_service.rb
+++ app/services/foreman_gutterball/gutterball_service.rb
@@ -1,56 +1,39 @@
 require 'rest_client'
 
 module ForemanGutterball
-  class GutterballResource < ::Katello::HttpResource
+  class GutterballService < ::Katello::HttpResource
     cfg = SETTINGS.with_indifferent_access
-    url = cfg[:url]
-    self.prefix = 'gutterball/reports'
+    url = cfg[:foreman_gutterball][:url]
+    self.prefix = URI.parse(url).path
     self.site = url.gsub(prefix, '')
     self.consumer_secret = cfg[:oauth_consumer_secret]
     self.consumer_key = cfg[:oauth_consumer_key]
     self.ca_cert_file = cfg[:ca_cert_file]
-
-    def self.logger
-      ::Logging.logger['gutterball_service']
-    end
 
     def self.default_headers
       { 'accept' => 'application/json',
         'accept-language' => I18n.locale,
         'content-type' => 'application/json' }
     end
-  end
 
-  class GutterballService < GutterballResource
-    # rubocop:disable Style/CyclomaticComplexity
-    def initialize(_options = {})
+    def self.logger
+      ::Logging.logger['gutterball_service']
     end
 
-    def available_reports
+    def report_details(report_key = nil)
+      report_path = "#{prefix}reports/#{report_key}"
       get_reports(report_path, default_headers)
     end
 
-    def consumer_status(params = nil)
-      get_reports(report_path('/consumer_status', params), default_headers)
-    end
-
-    def consumer_trend(params = nil)
-      get_reports(report_path('/consumer_trend', params), default_headers)
-    end
-
-    def status_trend(params = nil)
-      get_reports(report_path('/status_trend', params), default_headers)
-    end
-
-    def report_path(report_key = nil, params = nil)
-      "#{prefix}#{report_key}#{params}"
+    def run_reports(report_key, query_params = nil)
+      report_path = "#{prefix}reports/#{report_key}/run?#{query_params.to_query}"
+      get_reports(report_path, default_headers)
     end
 
     private
 
     def get_reports(path, headers = {})
-      client = GutterballResource.rest_client(Net::HTTP::Get, :get, path)
-      JSON.parse client.get(headers)
+      self.class.get(path, headers)
     end
   end
 end
