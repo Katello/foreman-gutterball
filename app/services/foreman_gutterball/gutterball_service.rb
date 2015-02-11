6aa19a4f2b8419b176ee490b6144dababe824c25
Adam Price <komidore64@gmail.com>
2015-02-11 13:58:37 -0500

    adding site back

--- app/services/foreman_gutterball/gutterball_service.rb
+++ app/services/foreman_gutterball/gutterball_service.rb
@@ -3,7 +3,8 @@ module ForemanGutterball
     def initialize
       cfg = SETTINGS.with_indifferent_access
       url = cfg['foreman_gutterball']['url']
-      self.prefix = URI.parse(url).to_s
+      self.prefix = URI.parse(url).path
+      self.site = url.gsub(prefix, '')
       self.consumer_secret = cfg[:oauth_consumer_secret]
       self.consumer_key = cfg[:oauth_consumer_key]
       self.ca_cert_file = cfg[:ca_cert_file]
